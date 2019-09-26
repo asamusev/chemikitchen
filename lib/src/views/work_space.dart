e458feeb98ff995ec08ba740e947b44acaa6f1f5
maxnemoy <maxnemoy@gmail.com>
2019-09-26 15:36:40 +0800

    menu zone

--- lib/src/views/work_space.dart
+++ lib/src/views/work_space.dart
@@ -8,11 +8,38 @@ class WorkSpace extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: <Widget>[
-        Expanded(child: SpawnArea()),
-        Expanded(child: TargetArea(title: "SingleElement", color: Colors.orange[400], elements: Provider.of<ChemiData>(context).singleElements,)),
-        Expanded(child: TargetArea(title: "ComplexElements", color: Colors.orange[800], elements: Provider.of<ChemiData>(context).complexElement,)),
-      ],),
+      body: Row(
+        children: <Widget>[
+          Expanded(child: Container(
+            color: Colors.orange[100],
+            child: Column(
+              //mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text("Score: 0"),
+                ),
+                Spacer(),
+                IconButton(icon: Icon(Icons.find_replace), onPressed: (){},),
+                IconButton(icon: Icon(Icons.hot_tub), onPressed: (){},),
+                IconButton(icon: Icon(Icons.cloud), onPressed: (){},),
+                IconButton(icon: Icon(Icons.radio_button_checked), onPressed: (){},),
+                IconButton(icon: Icon(Icons.error_outline), onPressed: (){},),
+                Spacer(),
+              ],
+            ),
+          ),),
+          Expanded(
+              flex: 15,
+              child: Column(children: <Widget>[
+              Expanded(child: SpawnArea()),
+              Expanded(child: TargetArea(title: "SingleElement", color: Colors.orange[400], elements: Provider.of<ChemiData>(context).singleElements,)),
+              Expanded(child: TargetArea(title: "ComplexElements", color: Colors.orange[800], elements: Provider.of<ChemiData>(context).complexElement,)),
+            ],),
+          ),
+        ],
+      ),
     );
   }
 }
